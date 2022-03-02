09c90a6bf4d280ea0d1f84a3dd22d9e5f198c361
Emerson-Pombo <emersonpombo5@gmail.com>
2022-03-02 14:45:34 -0300

    testing sign up button

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,18 +1,21 @@
 
-
 describe('home page', () => {
     it('app deve estar online', () => {
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
     })
-
     it('testing responsiveness', () => {
         
         cy.viewport(320, 568);
 
         it('has a visible mobile profile picture', () => {
             cy.get('header img ').should('be.visible')
+            
         })
+    })
+    it('testing sign up button', () =>{
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') 
     })
 })
